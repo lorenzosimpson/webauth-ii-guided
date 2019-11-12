7e8a6a2e71542a6b3d24848a18266e20e996423c
Lorenzo Simpson <lorenzosimp@gmail.com>
2019-11-12 12:59:51 -0500

    sets up session

--- api/server.js
+++ api/server.js
@@ -1,15 +1,26 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
+const session = require('express-session')
 
 const authRouter = require('../auth/auth-router.js');
 const usersRouter = require('../users/users-router.js');
 
 const server = express();
 
+const sessionConfiguration = {
+  name: 'booger',
+  secret: process.env.COOKIE_SECRET || 'is it secret? is it safe?',
+  cookie: {
+    maxAge: 1000 * 60 * 60, // 1hr
+    secure: process.env.NODE_ENV === 'development' ? false : true, // send cookie over unsecure http?
+  }
+}
+
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
+server.use(session(sessionConfiguration))
 
 server.use('/api/auth', authRouter);
 server.use('/api/users', usersRouter);
